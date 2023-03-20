272491ad486d201bf0f0d55c9fef35de16f39619
Agata Godlewska <aga.skrzeczkowska@gmail.com>
2023-03-20 20:47:42 +0100

    initial commit

---
+++ models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine(
+    "mysql+pymysql://root:Yomen1993@127.0.0.1:3306/portal"
+)
+
+Base = declarative_base(bind=engine)
